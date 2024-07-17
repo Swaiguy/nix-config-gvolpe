22d4dbb62083d530b540ae56130128f910ec6c68
Swaiguy <126988847+Swaiguy@users.noreply.github.com>
2024-07-17 18:24:51 +0200

    Add files via upload

--- system/configuration.nix
+++ system/configuration.nix
@@ -34,11 +34,36 @@ in
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+   time.timeZone = "Africa/Johannesburg";
 
-  # Set your time zone.
-  time.timeZone = "Europe/Warsaw";
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_ZA.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_ZA.UTF-8";
+    LC_IDENTIFICATION = "en_ZA.UTF-8";
+    LC_MEASUREMENT = "en_ZA.UTF-8";
+    LC_MONETARY = "en_ZA.UTF-8";
+    LC_NAME = "en_ZA.UTF-8";
+    LC_NUMERIC = "en_ZA.UTF-8";
+    LC_PAPER = "en_ZA.UTF-8";
+    LC_TELEPHONE = "en_ZA.UTF-8";
+    LC_TIME = "en_ZA.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "za";
+    xkbVariant = "";
+  };
 
   users.users.kry = {
     isNormalUser = true;
@@ -88,7 +113,7 @@ in
     };
   };
 
-  users.extraGroups.vboxusers.members = [ "gvolpe" ];
+  users.extraGroups.vboxusers.members = [ "kry" ];
 
   security.rtkit.enable = true;
 
@@ -209,5 +234,5 @@ in
 
     };
     };
-
+        system.stateVersion = "24.05";
 }
